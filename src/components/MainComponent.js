23098ce0022e8c63470e3b6769219d82dbc77514
Kunal Chaudhary <kunal.ch1812@gmail.com>
2019-06-17 01:48:38 +0530

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -31,13 +32,24 @@ class App extends Component {
                 />
             );
         }
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id===parseInt(match.params.dishId))[0]}
+                comments={this.state.comments.filter((comment) => comment.dishId ===parseInt(match.params.dishId) )}
+                />
+            );
+        }
+        
+        
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/menu/:dishId' component={DishWithId} />
                     <Route path='/contactus' component={Contact} />
+                    <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                     <Redirect to="/home" />
                 </Switch>
 
