1015e496228995bf57c759cdcd12d9148c36b292
Kunal Chaudhary <kunal.ch1812@gmail.com>
2019-06-23 22:06:37 +0530

    Combining Reducers1

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,6 +1,6 @@
 import { DISHES } from '../shared/dishes';
 
-export const Dishhes = ( state = DISHES,action) => {
+export const Dishes = ( state = DISHES,action) => {
     switch(action.type){
         default :
                 return state;
